1fa67c4300472f12f7007754e9e1c57c5c88fa26
“marte” <marte_hobbelink@hotmail.com>
2020-10-01 10:43:16 +0200

    Add Guest object

---
+++ src/nl/hotel/Guest.java
@@ -0,0 +1,47 @@
+package nl.hotel;
+
+public class Guest {
+    private String voornaam;
+    private String achternaam;
+    private int telefoonnummer;
+    private String email;
+
+    public Guest(String voornaam, String achternaam, int telefoonnummer, String email) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.telefoonnummer = telefoonnummer;
+        this.email = email;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public int getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(int telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
